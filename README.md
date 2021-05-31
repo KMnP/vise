1db5742a718a8447caaac3fea8c3d876e64a75fc
KMnP <mj493@cornell.edu>
2021-05-30 22:14:11 -0400

    Initial Commits

---
+++ README.md
@@ -0,0 +1,72 @@
+Exploring Visual Engagement Signals for Representation Learning
+================================================================
+
+<h4>
+Menglin Jia, Zuxuan Wu, Austin Reiter, Claire Cardie, Serge Belongie and Ser-Nam Lim
+</br>
+<span style="font-size: 14pt; color: #555555">
+Cornell University, Facebook AI
+</span>
+</h4>
+<hr>
+
+**arXiv** https://arxiv.org/abs/2104.07767
+
+**VisE** is a pretrianing approach which leverage **Vis**ual **E**ngagement clues as supervisory signals. Given the same image, visual engagement provide semantically and contextually richer information than conventional recognition and captioning tasks.  VisE transfers well to subjective downstream computer vision tasks like emotion recognition or political bias classification.
+
+
+
+<img src="images/teaser.png" alt="common supervisory signals" style="zoom:60%;" />
+
+<img src="images/teaser.gif" alt="vise teaser" style="zoom:75%;" />
+
+
+
+## 💬 Loading models with torch.hub
+
+Get the pretrained ResNet-50 models from VisE *in one line*!  
+
+:exclamation:**NOTE**: This is a torchvision-like model (all the layers before the last global average-pooling layer.). Given a batch of image tensors with size ``(B, 3, 224, 224)``, the provided models produce spatial image features of shape ``(B, 2048, 7, 7)``, where ``B``  is the batch size.
+
+#### VisE-250M (ResNet-50)
+
+This model is pretrained with 250 million public image posts.
+
+```python
+import torch
+model = torch.hub.load("KMnP/vise", "resnet50_250m", pretrained=True)
+```
+
+#### VisE-1.2M (ResNet-50)
+
+This model is pretrained with 1.23 million public image posts.
+
+```python
+import torch
+model = torch.hub.load("KMnP/vise", "resnet50_1m", pretrained=True)
+```
+
+
+
+## 💬 Citing VisE
+
+If you find VisE useful in your research, please cite the following publication.
+
+```
+@misc{jia2021vise,
+      title={Exploring Visual Engagement Signals for Representation Learning}, 
+      author={Menglin Jia and Zuxuan Wu and Austin Reiter and Claire Cardie and Serge Belongie and Ser-Nam Lim},
+      year={2021},
+      eprint={2104.07767},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+```
+
+## 💬 Acknowledgments
+
+We thank Marseille who was featured in the teaser photo. 
+
+## 💬 License
+
+VisE models are released under the CC-BY-NC 4.0 license. See [LICENSE](https://github.com/KMnP/vise/blob/master/LICENSE) for additional details.
---
+++ hubconf.py
@@ -0,0 +1,73 @@
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Optional list of dependencies required by the package
+dependencies = ['torch', 'torchvision']
+
+import torch
+import torchvision
+
+
+model_urls = {
+    'resnet50_1m': "https://cornell.box.com/shared/static/iewb4hlabndnxtkvkiojzxyk54y5uzxd.pth",
+    'resnet50_250m': "https://cornell.box.com/shared/static/b902w3tgd042stfocdxsf6nqv8egmbwn.pth",
+}
+
+
+def _resnet50(model_arch: str, pretrained: bool = False, **kwargs):
+    """
+    Args:
+        model_arch (str): specify which model file to download.
+        progress (bool): If True, displays a progress bar of the download to stderr.
+    """
+
+    # Create a torchvision resnet50 with randomly initialized weights.
+    model = torchvision.models.resnet50(pretrained=False, **kwargs)
+
+    # Get the model before the global aver-pooling layer.
+    model = torch.nn.Sequential(*list(model.children())[:-2])
+
+    if pretrained:
+        model.load_state_dict(torch.hub.load_state_dict_from_url(
+            model_urls[model_arch], progress=True)
+        )
+    return model
+
+
+def resnet50_1m(pretrained: bool = False, **kwargs):
+    """
+    Constructs a ResNet-50 model pre-trained on 1.2M visual engagement
+    data in `"Exploring Visual Engagement Signals for Representation Learning"
+    <https://arxiv.org/abs/2104.07767>`_
+
+    This is a torchvision-like model. Given a batch of image tensors with size
+    ``(B, 3, 224, 224)``, this model computes spatial image features of size
+    ``(B, 2048, 7, 7)``, where B = batch size.
+
+    Args:
+        progress (bool): If True, displays a progress bar of the download to stderr.
+    """
+    model = _resnet50(
+        model_arch="resnet50_1m", pretrained=pretrained, **kwargs
+    )
+    return model
+
+
+def resnet50_250m(pretrained: bool = False, **kwargs):
+    """
+    Constructs a ResNet-50 model pre-trained on 250M visual engagement
+    data in `"Exploring Visual Engagement Signals for Representation Learning"
+    <https://arxiv.org/abs/2104.07767>`_
+
+    This is a torchvision-like model. Given a batch of image tensors with size
+    ``(B, 3, 224, 224)``, this model computes spatial image features of size
+    ``(B, 2048, 7, 7)``, where B = batch size.
+
+    Args:
+        progress (bool): If True, displays a progress bar of the download to stderr.
+    """
+    model = _resnet50(
+        model_arch="resnet50_250m", pretrained=pretrained, **kwargs
+    )
+    return model
+
